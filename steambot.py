76d72bf655a72c11a636aa71b0bcd45361d3667a
matt <lipsius.m@gmail.com>
2022-03-09 15:12:15 -0700

    casted items to a set to get unique titles

--- steambot.py
+++ steambot.py
@@ -3,6 +3,7 @@ import requests
 import json
 import sys
 import feedparser
+#import game
 from bs4 import BeautifulSoup as bs
 
 #nabbin up them current specials
@@ -17,11 +18,20 @@ def main():
 	current_top_sellers()
 
 def current_top_sellers():
+	#make a set
+	set_of_games = {}
+	set_of_games = set(set_of_games)
 	feed = requests.get(store_url)
 	soup = bs(feed.text, 'html.parser')
 
-	
+	#some tags reoccur on the highlighted portions of the page
+	#so i have to cast to a set to not print duplicate items
 	for i in soup.find_all(class_='tab_item_name'):
+		set_of_games.add(i)
+
+	# for game in set_of_games:
+	# 	print (game)
+	for i in soup.find_all(class_='discount_price'):
 		print (i)
 
 	#class tab_item_name
@@ -34,9 +44,6 @@ def current_top_sellers():
 #def post_to_disc():
 	#sales = {}
 
-
-#grab sales from steam store
-#def grab_sales():
 
 #if this application was run directly, run it
 if __name__ == "__main__":
