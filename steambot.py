bb0b4e0193ec8bc1240619f25a7428e96dc181b3
matt <lipsius.m@gmail.com>
2022-03-08 23:21:31 -0700

    successfully scraping the store page, formatted with beautiful soup. need to find a way to store the intended data

---
+++ steambot.py
@@ -0,0 +1,41 @@
+import time
+import requests
+import json
+import sys
+import feedparser
+from bs4 import BeautifulSoup as bs
+
+#nabbin up them current specials
+store_url= 'https://store.steampowered.com/specials/#p=0&tab=TopSellers'
+
+test = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+
+#this is the class i need to grab for the ETA of next sale
+#<span class="huge-countdown" id="js-sale-countdown" data-target="1647277200000">05 : 12 : 53 : 46</span>
+#figure out how to grab specific headers
+def main():
+	 current_top_sellers()
+
+def current_top_sellers():
+	feed = requests.get(store_url)
+	soup = bs(feed.text)
+	soup = soup.prettify()
+	print(soup)
+	#need 
+	#class tab_item_name
+	#class discount_pct
+	#class discount_original_price
+	#class discount_final_price
+	#anchor tag href
+
+#send the items from the sale to the discord webhook id
+#def post_to_disc():
+	#sales = {}
+
+
+#grab sales from steam store
+#def grab_sales():
+
+#if this application was run directly, run it
+if __name__ == "__main__":
+    main()
