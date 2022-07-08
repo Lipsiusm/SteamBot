86b2786f397c4223379337222eed5f91ab7cbfe5
Matthew Lipsius <lipsius.m@gmail.com>
2022-07-08 10:33:21 -0600

    code clean up, removing unused imports/scraping for proxies

--- steambot.py
+++ steambot.py
@@ -8,19 +8,21 @@ with open("../bot_info.json", "r") as webhook_file:
     bot_info  = json.load(webhook_file)
     intents = discord.Intents.default()
     client = discord.Client()
+    DIG_CHANNEL = bot_info['DIG_CHANNEL']
     TOKEN = bot_info["TOKEN"]
 
 
 
 @client.event
 async def on_message(message):
-
     msg = message.content
 
     if msg.lower() == "!steamsales":
-        response = cdn_top_sellers()
-        send_cdn_info(response)
+        response = top_sellers()
+        send_info(response)
         time.sleep(5)
 
-    if msg.lower() == "!steamsales"
+    if msg.lower() == "!steambot":
+        response = "!steamsales to see top selling Steam games in Canada\n"
+        await message.channel.send(response)
 client.run(TOKEN)
